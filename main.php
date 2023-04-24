ede165d56b58c62ef008877794ca0f671cdf639a
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-04-25 00:33:03 +0330

    Update main.php

--- main.php
+++ main.php
@@ -42,7 +42,52 @@ function replaceIpAddresses($url , $option)
     $config_type = "";
 	
 	$clean_ips = clean_ip()[$option];
-	$config_array = [];
+	$config_array = [];<?php
+
+$result = array(
+        "last_update" => "",
+        "last_timestamp" => 0,
+        "ipv4" => array(),
+        "ipv6" => array()
+    );
+    
+$ips = [];
+
+function resolve (){
+    $data = json_decode(file_get_contents("providers.json") , true);
+
+    foreach ($data as $key => $url_array){
+    foreach ($url_array as $url){
+        $dns_array = dns_get_record($url ,  DNS_A);
+        foreach ($dns_array as $dns_data){
+            $ips[] = [
+                "ip" => $dns_data['ip'],
+                "operator" => $key,
+                "provider" => implode(".", array_slice(explode(".", $dns_data['host']), 1)),
+                "created_at" => strtotime(date('Y-m-d H:i:s')),
+            ];
+        }
+    }
+}
+
+$result = array(
+        "last_update" => date('Y-m-d H:i:s'),
+        "last_timestamp" => strtotime(date('Y-m-d H:i:s')),
+        "ipv4" => $ips,
+        "ipv6" => array()
+    );
+    
+return $result;
+}
+
+function save(){
+    $result = resolve();
+    file_put_contents('list.json', json_encode($result, JSON_PRETTY_PRINT));
+}
+
+save();
+
+?>
 	
     foreach ($lines as $config) {
         if (strpos($config, "vmess://") === 0) {
